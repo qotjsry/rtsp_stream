ed6526865a7d9a0a24f2fe12419c7e1ddeb6b418
qotjsry <qotjsry@gmail.com>
2023-02-09 13:11:25 +0900

    file clean

--- node-rtsp-stream/app.js
+++ node-rtsp-stream/app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var rtspRouter = require('./routes/rtspStream');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/rtsp', rtspRouter);
 
 // catch 404 and forward to error handler
